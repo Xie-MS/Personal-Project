38bf8a5a69adcba766c3ca3572b5eadd13c985aa
XieMS <xms881126@gmail.com>
2022-09-19 11:49:17 +0800

    Environment settings

---
+++ github/src/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+root.render(
+  <BrowserRouter>
+     <Routes>
+     <Route path="/" element={<App />}>
+         {/* <Route index element={<Home />} /> */}
+        {/* <Route path="/Label Management" element={<App />} /> 
+        <Route path="/IssueList" element={<App />} /> 
+        <Route path="/Issue" element={<App />} /> 
+        <Route path="/NewIssue" element={<App />} />  */}
+       </Route>
+    </Routes>
+  </BrowserRouter> 
+
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
