6192b15a2d5a9f56a3ea03c14d488ed47e7b033e
XieMS <xms881126@gmail.com>
2022-09-20 18:06:27 +0800

    add Sprint1 Layout

--- github/src/App.tsx
+++ github/src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { Reset } from "styled-reset";
+import { Header } from "./stories/Header";
+
+import Headers from "./header";
 
 function App() {
   return (
     <>
       <Reset />
+      <Headers />
       <Outlet />
     </>
   );
