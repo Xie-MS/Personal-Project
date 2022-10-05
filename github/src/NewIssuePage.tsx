a002ab80366e9ce1fbe4d3662ee2069c7ad288c4
XieMS <xms881126@gmail.com>
2022-10-05 14:36:44 +0800

    NewIssuePage Layout Done

--- github/src/NewIssuePage.tsx
+++ github/src/NewIssuePage.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { useState, useEffect, useRef } from "react";
+
 import {
   TypographyIcon,
   QuoteIcon,
@@ -24,7 +26,16 @@ import {
 
 import UserImg from "../src/img/userImg.png";
 
+import AssigneeList from "./AssigneeList";
+import LabelList from "./LabelsList";
+
 function NewIssuePage() {
+  const [preview, setPreview] = useState(false);
+  const [listClose, setListClose] = useState(false);
+  const [assigneelist, setAssigneeList] = useState(false);
+  const [labelList, setLabelList] = useState(false);
+  const [renderData, setRenderData] = useState([]);
+
   return (
     <div className="md:block mt-6 px-4 relative lg:flex justify-evenly items-start lg:px-6 xl:flex xl:px-6">
       <div className="lg:relative flex justify-evenly items-start xl:flex relative">
@@ -41,16 +52,35 @@ function NewIssuePage() {
               <div className="md:mb-4 md:px-0 md:py-0 lg:mb-0 lg:px-2 lg:py-2 xl:mb-0 xl:px-2 xl:py-2">
                 <input
                   type="text"
-                  className="px-3 py-[5px] border-[1px] border-solid border-gray-400 bg-slate-50 rounded-md w-full"
+                  defaultValue="Title"
+                  className="px-3 py-[5px] border-[1px] border-solid border-gray-400 bg-slate-50 rounded-md w-full lg:focus:bg-white lg:focus:border-[#218bff] xl:focus:bg-white xl:focus:border-blue-400"
                 />
               </div>
               <div className="w-full">
                 <div className="md:w-full md:mt-0 md:mx-0 lg:mt-2 lg:mx-2 lg:flex lg:justify-between lg:items-end  xl:mt-2 xl:mx-2 xl:flex xl:justify-between xl:items-end">
                   <div className="lg:flex lg:justify-between lg:items-end md:w-full xl:flex xl:justify-between xl:items-end ">
-                    <button className="py-2 px-5 md:border-[1px] border-solid border-gray-400 md:w-[50%] lg:w-auto lg:border-[1px] lg:border-b-[0px] lg:border-gray-200 lg:rounded-t-md  xl:w-auto xl:border-[1px] xl:border-b-[0px] xl:border-gray-200 xl:rounded-t-md  ">
+                    <button
+                      className={`${
+                        preview
+                          ? "border-none bg-white"
+                          : "border-gray-400 bg-slate-50"
+                      } py-2 px-5 md:border-[1px] border-solid md:w-[50%] lg:w-auto lg:border-[1px] lg:border-b-[0px] lg:border-gray-200 lg:rounded-t-md xl:w-auto xl:border-[1px] xl:border-b-[0px] xl:border-gray-200 xl:rounded-t-md`}
+                      onClick={() => {
+                        setPreview(false);
+                      }}
+                    >
                       <p className="lg:text-sm xl:text-sm">Write</p>
                     </button>
-                    <button className="py-2 px-5 md:border-t-[1px] md:border-r-[1px] md:border-b-[1px] border-solid border-gray-400 bg-slate-50 md:w-[50%] lg:w-auto lg:border-[0px] lg:bg-white lg:border-b-[1px] lg:border-solid lg:border-gray-200  xl:w-auto xl:border-[0px] xl:bg-white xl:border-b-[1px] xl:border-solid xl:border-gray-200">
+                    <button
+                      className={`${
+                        preview
+                          ? "border-gray-200 border-t-[1px] border-r-[1px] border-l-[1px] border-b-[0px] bg-slate-50 rounded-t-md"
+                          : "bg-white border-b-[1px] border-solid border-gray-200"
+                      } py-2 px-5 md:border-t-[1px] md:border-r-[1px] md:border-b-[1px] border-solid border-gray-400 md:w-[50%] lg:w-auto xl:w-auto`}
+                      onClick={() => {
+                        setPreview(true);
+                      }}
+                    >
                       <p className="lg:text-sm xl:text-sm">Preview</p>
                     </button>
                   </div>
@@ -141,11 +171,16 @@ function NewIssuePage() {
                 </div>
               </div>
               <div className="my-2 lg:mx-2 xl:mx-2">
-                <div className="lg:h-[230px] lg:px-0 lg:py-0 border-[1px] border-solid border-gray-400 bg-slate-100 rounded-md w-full relative md:border-0 px-2 py-2 md:h-[200px] xl:h-[230px] xl:px-0 xl:py-0">
+                <div
+                  className={`${
+                    preview ? "hidden" : "block"
+                  } lg:h-[230px] lg:px-0 lg:py-0 border-[1px] border-solid border-gray-400 bg-slate-100 rounded-md w-full relative md:border-0 px-2 py-2 md:h-[200px] xl:h-[230px] xl:px-0 xl:py-0`}
+                >
                   <div>
                     <textarea
                       cols="30"
                       rows="10"
+                      defaultValue="Leave a comment"
                       className="md:h-[200px] px-2 py-2 border-[1px] md:border-b-[0px] border-solid border-gray-400 bg-slate-100 rounded-md w-full lg:focus:bg-white lg:border-b-[1px] border-t-[0px] border-r-[0px] border-l-[0px] lg:border-dashed lg:h-[200px] lg:rounded-b-[0px] xl:focus:bg-white xl:border-dashed xl:h-[200px] xl:rounded-b-[0px]"
                     />
                   </div>
@@ -161,7 +196,11 @@ function NewIssuePage() {
                     </button>
                   </div>
                 </div>
-                <div className="px-2 pb-2 w-full h-[191px] border-b-[2px] border-solid border-[#d0d7de] hidden">
+                <div
+                  className={`${
+                    preview ? "block" : "hidden"
+                  } px-2 pb-2 w-full h-[191px] border-b-[2px] border-solid border-[#d0d7de]`}
+                >
                   <p className="text-sm">Nothing to preview</p>
                 </div>
                 <div className="md:hidden">
@@ -199,7 +238,7 @@ function NewIssuePage() {
         </div>
       </div>
       <div className="md:w-full lg:w-[240px] xl:w-[256px]">
-        <div className="xl:flex justify-between items-center mb-[10px]">
+        <div className=" xl:flex justify-between items-center mb-[10px] relative">
           <div className="md:pt-4 lg:pt-0 xl:pt-0">
             <p className="py-1 text-xs mb-1 hover:text-[#0969da]">Assignees</p>
             <p className="text-xs hidden justify-start items-center">
@@ -209,49 +248,39 @@ function NewIssuePage() {
               <img
                 src={UserImg}
                 alt=""
-                className="w-[5%] rounded-full mr-1 lg:w-[5%] xl:w-[5%]"
+                className="md:w-[5%] rounded-full mr-1 lg:w-[9%] xl:w-[9%]"
               />
               <p className="text-xs font-semibold">Xie-MS</p>
             </button>
+            <AssigneeList
+              listClose={listClose}
+              setListClose={setListClose}
+              assigneelist={assigneelist}
+              setAssigneeList={setAssigneeList}
+            />
           </div>
-          <div className="pt-4">
+          <div
+            className="pt-4 cursor-pointer"
+            onClick={() => {
+              setAssigneeList(true);
+              setListClose(true);
+            }}
+          >
             <GearIcon size={16} />
           </div>
         </div>
-        <div className="bg-black opacity-60 top-0 bottom-0 left-0 right-0 hidden"></div>
-        <ul className="md:hidden md:top-[7.5%] md:left-[2.2%] md:bottom-[12.5%] text-sm md:w-[95.5%] lg:hidden xl:absolute top-0 bottom-0 bg-white border-[1px] border-solid border-gray-300 rounded-xl w-[275px] hidden">
-          <li className="xl:px-4 py-4 text-xs font-semibold flex justify-between items-center sm:font-semibold sm:px-4 sm:py-4">
-            <p>Assign up to 10 people to this issue</p>
-            <p>X</p>
-          </li>
-          <li className="xl:px-2 py-2 border-t-[1px] border-solid border-gray-300 md:px-4 md:py-4">
-            <input
-              type="text"
-              defaultValue="Type or choose a user"
-              className="xl:py-[5px] px-3 bg-white border-[1px] border-solid border-gray-300 rounded-xl text-sm w-full"
-            />
-          </li>
-          <li className="xl:py-[7px] px-4 text-xs border-t-[1px] border-solid border-gray-300 flex justify-start items-center md:px-[10px] md:bg-slate-100 md:font-semibold">
-            Suggeations
-          </li>
-          <li className="xl:py-[7px] px-4 border-t-[1px] border-solid border-gray-300 text-xs flex justify-start items-center md:pl-5 md:pr-2 md:py-4">
-            <div>
-              <CheckIcon size={16} className="mr-2" />
-            </div>
-            <div className="xl:flex items-center justify-start">
-              <img
-                src={UserImg}
-                alt=""
-                className="xl:w-[4%] rounded-full mr-2"
-              />
-              <div className="xl:flex items-center justify-center">
-                <p className="xl:mr-2 font-semibold">Xie-MS</p>
-                <p>xms_7104</p>
-              </div>
-            </div>
-          </li>
-        </ul>
-        <div className="mt-4 pt-4 border-t-[1px] border-solid border-gray-200 flex justify-between items-center">
+        <div
+          className={`${
+            listClose ? "block" : "hidden"
+          } md:bg-black md:opacity-60 top-0 bottom-0 left-0 right-0 fixed md:z-10`}
+          onClick={() => {
+            setListClose(false);
+            setAssigneeList(false);
+            setLabelList(false);
+          }}
+        />
+
+        <div className="mt-4 pt-4 border-t-[1px] border-solid border-gray-200 flex justify-between items-center relative">
           <div>
             <p className="py-1 text-xs mb-1 hover:text-[#0969da]">Labels</p>
             <p className="text-xs hidden justify-start items-center">Noneyet</p>
@@ -259,10 +288,22 @@ function NewIssuePage() {
               abcde
             </button>
           </div>
-          <div>
+          <div
+            className="pt-4 cursor-pointer"
+            onClick={() => {
+              setLabelList(true);
+              setListClose(true);
+            }}
+          >
             <GearIcon size={16} />
           </div>
+          <LabelList
+            setListClose={setListClose}
+            labelList={labelList}
+            setLabelList={setLabelList}
+          />
         </div>
+
         <div className="mt-4 pt-4 border-t-[1px] border-solid border-gray-200 flex justify-between items-center">
           <div>
             <p className="py-1 text-xs mb-1 hover:text-[#0969da]">Projects</p>
